42ea12829a794ab776d50b4bbfe005d1597a92a9
IAsWell <jyang1737@gmail.com>
2016-10-18 16:25:50 -0400

    finished work 7

--- get_averages.py
+++ get_averages.py
@@ -4,8 +4,10 @@ f = "discobandit.db"
 
 db = sqlite3.connect(f)
 
+#needs 2 cursors so that fxns do not interfere
 c = db.cursor()
 d = db.cursor()
+
 def calcAvg(id):
     q = "SELECT mark FROM courses WHERE courses.id = " + str(id) + ";"
     data = d.execute(q)
@@ -23,28 +25,5 @@ for record in data:
     
     print "Name: %s, ID: %d, Average: %f"%(record[0], record[1], calcAvg(record[1]))
 
-
-'''
-sum = 0.0
-ctr = 0
-name = ""
-id = 0
-newName = 0
-
-for record in data:
-    if (newName == 0):
-        sum = 0.0
-        ctr = 0
-        name = record[0]
-        id = record[1]
-        newName = 1
-    elif (name == record[0]):
-        print record[2]
-        sum += record[2]
-        ctr += 1
-    else:
-        print "Name: %s, ID: %d, Average: %f"%(name, id, sum/ctr)
-        newName = 0
-    
-
-'''
+db.commit()
+db.close()
